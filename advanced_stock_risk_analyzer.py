848d82d640929fbd4e3e9ce414612926d11b6fe7
A-S1723 <arjunsinghchelsea@gmail.com>
2026-07-29 17:09:35 +0100

    Add files via upload

--- advanced_stock_risk_analyzer.py
+++ advanced_stock_risk_analyzer.py
@@ -32,7 +32,7 @@ analyzer = SentimentIntensityAnalyzer()
 
 def get_news_headlines(ticker, max_items=10):
     def get_news_headlines(ticker, max_items=10):
-    api_key = Wr4D8qAmmOPADUCV5uSiSIy0SXSetIak
+    api_key = "Wr4D8qAmmOPADUCV5uSiSIy0SXSetIak"
     url = f"https://financialmodelingprep.com/api/v3/stock_news?tickers={ticker}&limit={max_items}&apikey={api_key}"
 
     try:
